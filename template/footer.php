6695b6b86d4ac2861b73fe40de89a77f0acf0d8f
jenkings <jenkings@jenkings-Nitro-AN16-41>
2024-01-10 21:29:35 +0100

    updated link

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,7 @@
 <div class="insert-post-ads1" style="margin-top:20px;">
 
 <footer>
-	Powered by <a href='https://jan-skoda.cz' target="_blank">PSL - CMS</a> by <a href='https://jan-skoda.cz' target="_blank">Jan Škoda</a>
+	Powered by <a href='https://psl-cms.jan-skoda.cz' target="_blank">PSL - CMS</a> by <a href='https://jan-skoda.cz' target="_blank">Jan Škoda</a>
 </footer>
 
 
